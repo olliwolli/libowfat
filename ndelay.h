9841a7ab65e9925003bf778cb96b3eec6f8dd4d8
Oliver Ripka <code@bockcay.de>
2010-07-09 17:04:16 +0100

    Initial commit

---
+++ ndelay.h
@@ -0,0 +1,8 @@
+/* this header file comes from libowfat, http://www.fefe.de/libowfat/ */
+#ifndef NDELAY_H
+#define NDELAY_H
+
+int ndelay_on(int);
+int ndelay_off(int);
+
+#endif
---
+++ openreadclose.h
@@ -0,0 +1,9 @@
+/* this header file comes from libowfat, http://www.fefe.de/libowfat/ */
+#ifndef OPENREADCLOSE_H
+#define OPENREADCLOSE_H
+
+#include "stralloc.h"
+
+int openreadclose(const char *filename,stralloc *buf,size_t initiallength);
+
+#endif
